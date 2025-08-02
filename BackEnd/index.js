08418107b28abc8693bf5175aa56b497329e7cf8
isha-web1 <isha.web1@gmail.com>
2025-08-02 20:21:11 +0600

    add admin statistics & refactore index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,8 +1,28 @@
-const express = require('express');
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
 const mongoose = require("mongoose");
-require("dotenv").config();
-const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+// middleware
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173', ],
+    credentials: true
+}))
+
+// routes
+const bookRoutes = require('./src/books/book.route');
+const orderRoutes = require("./src/orders/order.route")
+const userRoutes =  require("./src/users/user.route")
+const adminRoutes = require("./src/stats/admin.stats")
+
+app.use("/api/books", bookRoutes)
+app.use("/api/orders", orderRoutes)
+app.use("/api/auth", userRoutes)
+app.use("/api/admin", adminRoutes)
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
@@ -13,8 +33,6 @@ async function main() {
 
 main().then(() => console.log("Mongodb connect successfully!")).catch(err => console.log(err));
 
-// yaP2IHXH2rf6BKk7
-// ishaweb1
 app.listen(port, () => {
-  console.log(`Book store server running on port ${port}`)
-})
+  console.log(`Book store application listening on port ${port}`);
+});
